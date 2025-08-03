714da0295b767ed59bbfcc5c3ba9deffba798798
adamsalimtidoo <119486996+adamsalimtidoo@users.noreply.github.com>
2025-08-03 11:27:23 +0000

    updated

---
+++ alx-project-0x04/interface/index.tsx
@@ -0,0 +1,18 @@
+//interfaces
+
+export interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
+
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+
+export interface PageRouteProps {
+  pageRoute: string
+}
+
